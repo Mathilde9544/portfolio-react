4d9e77bb04601f72d19a04254e3aece67c9ac8d1
mathilde renaud <renaud.mathilde95@hotmail.fr>
2025-01-07 12:40:04 +0100

    text

--- src/components/journey/journey.js
+++ src/components/journey/journey.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from "react";
+import { useState } from 'react';
 import './journey.css';
 import { Link } from "react-router-dom";
 import { GoDot } from "react-icons/go";
@@ -9,8 +9,11 @@ import garden from "../../assets/gardenease-favicon.ico";
 import skate from "../../assets/mouse.png";
 
 const Journey  = () => {
-  const [isHovered, setIsHovered] = useState(false);
-  console.log("isHovered state:", isHovered);
+  const [visibleProject, setVisibleProject] = useState(null);
+
+  const handleAboutClick = (project) => {
+    setVisibleProject(visibleProject === project ? null : project);
+  };
 
   return (
     <div className="parcours">
@@ -38,31 +41,65 @@ const Journey  = () => {
       <div className="projects">
         <h3 className="wagon-projects">My main projects at Le Wagon:</h3>
         <div className="project-cards">
-          <Link to="/gardenease" className="project-link">
-            <div className="project" id="gardenease-project"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}>
-              <img class="project-image" src={garden} alt="GardenEase"></img>
-              <h3 className="project-title">GardenEase</h3>
-            </div>
-          </Link>
-
-          <Link to="/" className="project-link">
-            <div className="project">
-              <img className="project-image" id="mouse" src={skate} alt="Adventure Exchange"></img>
-              <h3 className="project-title">Aventure Exchange</h3>
-            </div>
-          </Link>
-        </div>
-        <div className={`project-text ${isHovered ? "visible" : ""}`}>
-            <p>GardenEase is a website for people who would like
+          <div className="project-card">
+            <Link to="/gardenease" className="project-link">
+              <div className="project" id="gardenease-project">
+                <img class="project-image" src={garden} alt="GardenEase"></img>
+                <h3 className="project-title">GardenEase</h3>
+              </div>
+            </Link>
+            <p className="project-info" onClick={() => handleAboutClick("gardenease")}>
+              {visibleProject === "gardenease" ? "Hide" : "See description"}
+            </p>
+            {visibleProject === "gardenease" && (
+            <div className="project-text" id="project-text-gardenease">
+              <p>GardenEase is a website for people who would like
                 to start gardening at their place, but don't know how to start.<br/>
                 By understanding your preferences and home environment, GardenEase
                 provides tailored gardening projects with step-by-step care advices.
+
+                GardenEase was built during the 2 last weeks of the bootcamp.
               </p>
           </div>
-      </div>
+        )}
+          </div>
 
+          <div className="project-card">
+            <Link to="/" className="project-link">
+              <div className="project" id="adventurex-project">
+                <img className="project-image" id="mouse" src={skate} alt="Adventure Exchange"></img>
+                <h3 className="project-title">Aventure Exchange</h3>
+              </div>
+            </Link>
+            <p className="project-info" onClick={() => handleAboutClick("adventurex")}>
+              {visibleProject === "adventurex" ? "Hide" : "See description"}
+            </p>
+          </div>
+        </div>
+
+        {visibleProject === "gardenease" && (
+          <div className="project-text">
+            <p>GardenEase is a website for people who would like
+              to start gardening at their place, but don't know how to start.<br/>
+              By understanding your preferences and home environment, GardenEase
+              provides tailored gardening projects with step-by-step care advices.
+
+              GardenEase was built during the 2 last weeks of the bootcamp.
+            </p>
+          </div>
+        )}
+
+        {visibleProject === "adventurex" && (
+          <div className="project-text" id="project-text-adventurex">
+            <p>Adventure Exchange is a mobile web App. The concept is similar to the
+              AirBnB idea: it's a rental app. With Adventure Exchange, people could
+              rent or offer to rent sports items.
+
+              Adventure Exchange was built in 1 week and was my first project at Le Wagon.
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
